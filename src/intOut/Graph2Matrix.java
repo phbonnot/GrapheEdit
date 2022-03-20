cdfd4bc0ea932a9c1d7774fce13c0ece8b18d3ab
phbonnot <phbonnot@gmail.com>
2022-03-20 16:36:12 +0100

    ajouté sauvegarde sous forme d'une matrice d'adjacence

---
+++ src/intOut/Graph2Matrix.java
@@ -0,0 +1,38 @@
+package intOut;
+
+
+import modele.Arete;
+import modele.Graphe;
+import modele.Sommet;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Graph2Matrix {
+
+    Graphe graphe;
+
+    public Graph2Matrix(Graphe g){
+        this.graphe=g;
+
+    }
+
+    public void ecritureMatrice(File f){
+        try {
+            PrintWriter writer = new PrintWriter(f.getName());
+            int[][] mat = this.graphe.matriceAdjacence();
+            for (int i = 0; i < mat.length; i++) {
+                for (int j = 0; j < mat[i].length; j++) {
+                    writer.print(mat[i][j]);
+                }
+                writer.println("");
+            }
+            writer.close();
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+    }
+}
